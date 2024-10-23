1c490c5f1dcb0aef2beb2f4c27f4fce59b5ecd0e
Zhen Bi <zhenbi@csil.cs.ucsb.edu>
2024-10-22 17:31:27 -0700

    finished

--- hw2/src/screen/HomeScreen.js
+++ hw2/src/screen/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import {signOut} from "firebase/auth";
 import {
     StatusBar,
@@ -7,14 +7,42 @@ import {
     View,
     Button
 } from "react-native";
-
 import {auth} from "../../firebaseConfig"
+import { Avatar } from "react-native-paper";
+
 
 const HomeScreen = ({ navigation }) => {
-    const [userInfo, setUserInfo] =useState();
+    const [userInfo, setUserInfo] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+        if (user) {
+            setUserInfo({
+            displayName: user.displayName,
+            email: user.email,
+            image: user.photoURL,
+            });
+        } else {
+            setUserInfo(null);
+        }
+        });
+        return () => unsubscribe(); // Clean up the listener on unmount
+    }, []);
+
     return (
         <View style={styles.container}>
-        <Button title ="Sign Out" onPress = {async () => await signOut(auth)} />
+            {userInfo && (
+            <View style={styles.userInfoContainer}>
+            <Avatar.Image
+              source={{ uri: userInfo.image }}
+              size={100} // Adjust if needed
+              marginBottom = {30}
+            />
+            <Text>Welcome, {userInfo.displayName}!</Text>
+            <Text>Email: {userInfo.email}</Text>
+            </View>
+        )}
+        <Button title="Sign Out" onPress={async () => await signOut(auth)} />
         <StatusBar style="auto" />
         </View>
     );
@@ -26,6 +54,16 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+      },
+      userInfoContainer: {
+        alignItems: "center",
+        marginBottom: 20,
+      },
+      userImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,  
+        marginBottom: 10,
       },
 });
 
